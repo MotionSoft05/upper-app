baefdcec16a26134fa93b2973ed2f2cfb5c39e18
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-09-26 20:03:23 -0300

    Update licencia.jsx

--- src/components/dashboard/licencia.jsx
+++ src/components/dashboard/licencia.jsx
@@ -1,57 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import DatosFiscales from "./licenciaComponentes/datosFiscales";
+import LicenciaContratada from "./licenciaComponentes/LicenciaContratada";
+import DatosNegocio from "./licenciaComponentes/datosNegocio";
+import InformacionAdicional from "./licenciaComponentes/InformacionAdicional";
 
 function Licencia() {
+  const [showDatosNegocio, setShowDatosNegocio] = useState(false);
+  const [showDatosFiscales, setShowDatosFiscales] = useState(false);
+  const [showLicenciaContratada, setShowLicenciaContratada] = useState(false);
+  const [showInformacionAdicional, setShowInformacionAdicional] =
+    useState(false);
+
   return (
     <section className="px-8 py-12">
-      <div className="max-w-4xl mx-auto">
-        <div className="mb-8">
-          <h2 className="text-3xl font-extrabold text-gray-900">Licencia</h2>
-        </div>
-        <div className="bg-white p-6 shadow-md rounded-md">
-          {/* Datos Generales del negocio */}
-          <div className="mb-6">
-            <h3 className="text-xl font-semibold text-gray-800">
-              Datos Generales del Negocio
-            </h3>
-            <p>Nombre: Nombre del Negocio</p>
-            <p>Domicilio: Domicilio del Negocio</p>
-            <p>Teléfono: Teléfono de Contacto</p>
-            <p>Correo de Contacto: correo@example.com</p>
-          </div>
-
-          {/* Datos Fiscales */}
-          <div className="mb-6">
-            <h3 className="text-xl font-semibold text-gray-800">
-              Datos Fiscales
-            </h3>
-            <p>Información fiscal para emisión de factura</p>
-            {/* Agregar más información fiscal aquí */}
-          </div>
-
-          {/* Información de licencia contratada */}
-          <div className="mb-6">
-            <h3 className="text-xl font-semibold text-gray-800">
-              Información de Licencia Contratada
-            </h3>
-            <p>Fecha de Inicio: 01/01/2023</p>
-            <p>Fecha de Fin: 01/01/2024</p>
-            <p>Número de Licencias Adquiridas: 5</p>
-            <p>URL de Salón de Eventos en línea: Sí</p>
-            <p>URL de Directorio en línea: Sí</p>
-          </div>
-
-          {/* Información adicional */}
-          <div className="mb-6">
-            <h3 className="text-xl font-semibold text-gray-800">
-              Información Adicional
-            </h3>
-            <p>Datos de Contacto para Renovación y Upgrade de Licencia:</p>
-            <p>Nombre de Contacto: Nombre del Contacto</p>
-            <p>Teléfono de Contacto: Teléfono del Contacto</p>
-            <p>Correo de Contacto: correo_contacto@example.com</p>
-          </div>
-        </div>
+      <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        <ul class="flex flex-wrap -mb-px">
+          <li class="mr-2">
+            <button
+              onClick={() => {
+                setShowDatosNegocio(true);
+                setShowDatosFiscales(false);
+                setShowLicenciaContratada(false);
+                setShowInformacionAdicional(false);
+              }}
+            >
+              <div class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500">
+                <span className="ml-3">Datos generales del negocio</span>
+              </div>
+            </button>
+          </li>
+          <li class="mr-2">
+            <button
+              onClick={() => {
+                setShowDatosNegocio(false);
+                setShowDatosFiscales(true);
+                setShowLicenciaContratada(false);
+                setShowInformacionAdicional(false);
+              }}
+            >
+              <div class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">
+                <span className="ml-3">Datos fiscales</span>
+              </div>
+            </button>
+          </li>
+          <li class="mr-2">
+            <button
+              onClick={() => {
+                setShowDatosNegocio(false);
+                setShowDatosFiscales(false);
+                setShowLicenciaContratada(true);
+                setShowInformacionAdicional(false);
+              }}
+            >
+              <a
+                href="#"
+                class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              >
+                Información de licencia contratada
+              </a>
+            </button>
+          </li>
+          <li class="mr-2">
+            <button
+              onClick={() => {
+                setShowDatosNegocio(false);
+                setShowDatosFiscales(false);
+                setShowLicenciaContratada(false);
+                setShowInformacionAdicional(true);
+              }}
+            >
+              <a
+                href="#"
+                class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              >
+                Información adicional
+              </a>
+            </button>
+          </li>
+        </ul>
       </div>
+      <main>
+        <main className="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
+          <div className="main-content flex flex-col flex-grow p-4">
+            {showDatosNegocio && <DatosNegocio />}
+            {showDatosFiscales && <DatosFiscales />}
+            {showLicenciaContratada && <LicenciaContratada />}
+            {showInformacionAdicional && <InformacionAdicional />}
+          </div>
+        </main>
+      </main>
     </section>
   );
 }
