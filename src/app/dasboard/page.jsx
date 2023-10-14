075e701a1d5be7ded31c7369e43814f3441b6318
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-10-14 19:49:04 -0300

    Upper1

--- src/app/dasboard/page.jsx
+++ src/app/dasboard/page.jsx
@@ -8,10 +8,12 @@ import Licencia from "@/components/dashboard/licencia";
 import PantallasSalon from "@/components/dashboard/pantallasSalon";
 import Publicidad from "@/components/dashboard/publicidad";
 import Soporte from "@/components/dashboard/soporte";
+import Admin from "@/components/dashboard/admin";
 import Link from "next/link";
 import { useState } from "react";
 
 function DashBoard() {
+  const [showAdmin, setShowAdmin] = useState(true);
   const [showAltaEvento, setShowAltaEvento] = useState(true);
   const [showConsultaEvento, setShowConsultaEvento] = useState(false);
 
@@ -28,6 +30,7 @@ function DashBoard() {
     <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800 pt-16">
       <aside className="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-indigo-500">
         <Sidebar
+          setShowAdmin={setShowAdmin}
           setShowAltaEvento={setShowAltaEvento}
           setShowConsultaEvento={setShowConsultaEvento}
           setShowPantallaSalon={setShowPantallaSalon}
@@ -40,6 +43,7 @@ function DashBoard() {
       </aside>
       <main className="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
         <div className="main-content flex flex-col flex-grow p-4">
+          {showAdmin && <Admin />}
           {showAltaEvento && <AltaEventos />}
           {showConsultaEvento && <ConsultaModEvento />}
 
