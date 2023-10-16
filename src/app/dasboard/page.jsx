0db6e7e76ef3dcff54ae759943fd2cd472c4dba8
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-10-16 19:27:15 -0300

    SideBar

--- src/app/dasboard/page.jsx
+++ src/app/dasboard/page.jsx
@@ -25,10 +25,48 @@ function DashBoard() {
   const [showGuia, setShowGuia] = useState(false);
   const [showSoporte, setShowSoporte] = useState(false);
 
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    setTouchEndX(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    const touchDelta = touchStartX - touchEndX;
+    if (touchDelta > 50) {
+      // Deslizar hacia la izquierda, cerrar la barra lateral
+      setSidebarOpen(false);
+    } else if (touchDelta < -50) {
+      // Deslizar hacia la derecha, abrir la barra lateral
+      setSidebarOpen(true);
+    }
+  };
+
   return (
     // <!-- component -->
-    <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800 pt-16">
-      <aside className="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-indigo-500">
+    <div
+      className={`flex flex-row min-h-screen bg-gray-100 text-gray-800 pt-16 ${
+        sidebarOpen ? "md:ml-0" : "md:ml-64"
+      }`}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      <aside
+        className={`sidebar w-64 md:shadow transform ${
+          sidebarOpen ? "md:translate-x-0" : "-translate-x-full"
+        } transition-transform duration-150 ease-in bg-indigo-500`}
+      >
         <Sidebar
           setShowAdmin={setShowAdmin}
           setShowAltaEvento={setShowAltaEvento}
